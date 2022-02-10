1177c412d6660e4443c1194e6e92d2c4c43a4693
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-02-10 15:13:45 +0300

    feat: solve task of morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let count = expr.length / 10;
+let j = count
+const arr = []
+for (let i = 0; i < count; i++) {
+    arr.unshift(expr.slice(-10))
+     j = j - 1
+  expr = expr.slice(0, j * 10)
+}
+
+let res = arr.map(item => {
+    let array = []
+    if (item === '**********') {
+      array.push(' ')
+  } else {
+      for (let i = 0; i < item.length; i += 2) {
+      if ((item[i] + item[i + 1]) === '10') {
+        array.push('.')
+      } else if ((item[i] + item[i + 1]) === '11') {
+        array.push('-')
+      } 
+      }
+  }
+  return array.join('');
+})
+
+res = res.map(item => {
+    for(const i in MORSE_TABLE) {
+      if(i === item) {
+        item = MORSE_TABLE[i]
+    }
+  }
+  return item;
+})
+
+return res.join('')
 }
 
 module.exports = {
